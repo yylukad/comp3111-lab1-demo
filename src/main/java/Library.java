8852327b53e2885d9ccbe14cb4080d19b938d9a9
hkjj293 <chrishuihuihui@gmail.com>
2019-04-10 15:27:02 +0800

    haha

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,7 +7,7 @@ public class Library {
 	public static void main (String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
-			System.out.println("When there is a fire, commit and push.");
+			System.out.println("When diudiudiu there is a fire, commit and push.");
 	}
 	
     public boolean someLibraryMethod() {
