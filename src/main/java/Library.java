a8f6321f353b8d1f09348314f38246f3d51f3697
yylukad <yylukad@csl4wk36.win2k.cs.ust.hk>
2019-04-10 15:07:15 +0800

    new

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -8,7 +8,7 @@ public class Library {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
 			System.out.println("When there's a fire, commit and push.");
-		System.out.println("When there's a fire, commit and push.");
+		System.out.println("kuh.");
 	}
 	
     public boolean someLibraryMethod() {
