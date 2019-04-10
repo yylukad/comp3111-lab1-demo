ee8affedec5cac119e644ad46b12d77a936c327f
yylukad <47770445+yylukad@users.noreply.github.com>
2019-04-10 15:01:23 +0800

    Update Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,12 +7,10 @@ public class Library {
 	public static void main (String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
-			System.out.println("When there's a fire, commit and push.");
-		System.out.println("When there's a fire, commit and push.");
+			System.out.println("When there is a fire, commit and push.");
 	}
 	
     public boolean someLibraryMethod() {
         return true;
     }
 }
-//sdfhdashg
