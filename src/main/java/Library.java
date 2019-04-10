1a6d66d12e2410dfbd2c5bd417f97aff28b3222e
yylukad <yylukad@csl4wk36.win2k.cs.ust.hk>
2019-04-10 14:46:06 +0800

    nini

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -7,10 +7,11 @@ public class Library {
 	public static void main (String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
-			System.out.println("When there is a fire, commit and push.");
+			System.out.println("When there's a fire, commit and push.");
 	}
 	
     public boolean someLibraryMethod() {
         return true;
     }
 }
+//sdfhdashg
